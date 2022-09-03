9c54c581ef399f280ea4339572102ac5baf6c0e1
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-03 11:06:26 +0900

    7-8. 이분 검색

--- src/Main.java
+++ src/Main.java
@@ -1906,8 +1906,7 @@ public class Main {
 }
  */
 
-
-
+/* 6-7. 좌표 정렬
 public class Main {
 
     static class Point implements Comparable<Point> {
@@ -1943,3 +1942,38 @@ public class Main {
         }
     }
 }
+ */
+
+/* 7-8. 이분검색
+public class Main {
+
+    public int Solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0;
+        int rt = n-1;
+        while(lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if(arr[mid] > m) rt = mid - 1;
+            if(arr[mid] < m) lt = mid + 1;
+            if(arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.Solution(n, m, arr));
+    }
+}
+ */
