e98aecdd1d6c86e6286dcdc6972ae996ce8d796e
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-07 17:05:53 +0900

    7-1. 재귀함수(스택프레임)

--- src/Main.java
+++ src/Main.java
@@ -2073,3 +2073,23 @@ public class Main {
     }
 }
  */
+
+public class Main {
+
+    public void DFS(int n) {
+
+        if(n == 0) return;
+        else {
+            DFS(n-1);
+            System.out.print(n + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        T.DFS(n);
+    }
+}
