0011aee3ee8dd907d58ced79659aaa0c91d7527b
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-12 11:15:29 +0900

    7-3. 팩토리얼

--- src/Main.java
+++ src/Main.java
@@ -2096,6 +2096,7 @@ public class Main {
 }
  */
 
+/* 7-2. 이진수 출력 (재귀)
 public class Main {
 
     public void DFS(int n) {
@@ -2115,5 +2116,23 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         T.DFS(n);
+    }
+}
+ */
+
+public class Main {
+
+    public int DFS(int n) {
+        if(n == 1) return 1;
+        else {
+            return n * DFS(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.println(T.DFS(n));
     }
 }
