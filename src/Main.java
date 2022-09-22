8ea0a156e9984482bbb1af91642eb20644becbdf
KSH_coding <95729738+KSH-beginner@users.noreply.github.com>
2022-09-22 10:27:54 +0900

    7-13. 경로 탐색(DFS, 인접 리스트 - ArrayList)

--- src/Main.java
+++ src/Main.java
@@ -2430,3 +2430,45 @@ public class Main {
     }
 }
  */
+
+/* 7-13. 경로 탐색(DFS, 인접 리스트 - ArrayList)
+public class Main {
+
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+
+    public void DFS(int value) {
+        if(n == value) answer++;
+        else {
+            for(int nv : graph.get(value)) {
+                if(ch[nv] == 0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for(int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n+1];
+        for(int i = 0; i < m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
+ */
